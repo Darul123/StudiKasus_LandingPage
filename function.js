28e9f8b29685fd8147c070b27b4860c3bda20bba
Darul123 <darulh123@gmail.com>
2021-08-10 15:57:01 +0700

    landing page

---
+++ function.js
@@ -0,0 +1,7 @@
+function price(){
+    document.getElementById('price').scrollIntoView();
+}
+function btn(){
+    document.getElementById('btn').style.backgroundColor = "#fff";
+    document.getElementById('btn').style.color = "#51C4D3";
+}
